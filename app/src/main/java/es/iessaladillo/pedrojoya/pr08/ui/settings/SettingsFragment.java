f21039d95f5d8cf66390ed1c1b7caeea747a6535
Alejandro <alesitonightray@gmail.com>
2019-01-19 17:44:54 +0100

    change in the way the summary is changed

--- app/src/main/java/es/iessaladillo/pedrojoya/pr08/ui/settings/SettingsFragment.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr08/ui/settings/SettingsFragment.java
@@ -12,31 +12,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import es.iessaladillo.pedrojoya.pr08.R;
 
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener;
-
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        getPreferenceScreen().getSharedPreferences().
-                registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getPreferenceScreen().getSharedPreferences().
-                unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
     }
 
     @Override
@@ -47,9 +30,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        onSharedPreferenceChangeListener = this::updateSumary;
         setupToolbar(requireView());
-        setupSettings();
     }
 
     private void setupToolbar(View view) {
@@ -64,20 +45,4 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         }
     }
 
-    private void setupSettings() {
-        updateSumary(getPreferenceScreen().getSharedPreferences(),
-                getString(R.string.prefLoremType_key));
-    }
-
-    private void updateSumary(SharedPreferences sharedPreferences, String key) {
-        Preference preference = findPreference(key);
-
-        if (TextUtils.equals(preference.getKey(), getString(R.string.prefLoremType_key))) {
-            String[] values = getResources().getStringArray(R.array.prefLoremType_values);
-            preference.setSummary(TextUtils.equals(sharedPreferences.getString(
-                    getString(R.string.prefLoremType_key),
-                    getString(R.string.prefLoremType_defaultValue)),
-                    getString(R.string.prefLoremType_defaultValue)) ? values[0] : values[1]);
-        }
-    }
 }
